acf43b1271cc48f10a30cd65380fe0358a79e000
marcoalm32 <marcoalm25@gmail.com>
2023-06-04 09:15:14 -0300

    Implemented type and abilities card in modal

--- pokemon-react/src/components/poke-modal/PokeModal.tsx
+++ pokemon-react/src/components/poke-modal/PokeModal.tsx
@@ -7,6 +7,8 @@ import { FunctionComponent } from 'react';
 import { ImgModal } from './img-modal/ImgModal';
 import usePokemonStore from '../../store/pokemonStore';
 import { IconButton } from '@mui/material';
+import { TypeAndAbilities } from '../type-and-abilities/TypeAndAbilities';
+import { IAbilities, IType } from '../../models/PokemonDetail.model';
 
 import './PokeModal.scss';
 
@@ -26,7 +28,7 @@ export const PokeModal: FunctionComponent<any> = (props) => {
           open={props.open}
           onClose={verifyHasSelectedPokemon}
           className={pokeModal}
-          >
+        >
           <DialogContent className={`${pokeModal}__content`}>
             <div className={`${pokeModal}__content__closed`}>
               <IconButton onClick={verifyHasSelectedPokemon}>
@@ -36,13 +38,38 @@ export const PokeModal: FunctionComponent<any> = (props) => {
             <ImgModal
               name={props.pokemon?.name}
               img={props.pokemon?.sprites?.front_default}
-             />
+            />
+            <div className={`${pokeModal}__content__type_and_ability`}>
+              <div className={`${pokeModal}__content__type_and_ability__types`}>
+                <h3>Types</h3>
+                {props?.pokemon?.types.map((type: IType) => (
+                  <TypeAndAbilities
+                    key={type.slot}
+                    slot={type.slot}
+                    type={type.type}
+                  />
+                ))}
+              </div>
+              <div className={`${pokeModal}__content__type_and_ability__abilities`}>
+                <h3>Abilities</h3>
+                {props?.pokemon?.abilities.map((ability: IAbilities) => (
+                  <TypeAndAbilities
+                    key={ability.slot}
+                    ability={ability.ability}
+                    width={500}
+                  />
+                ))}
+              </div>
+            </div>
           </DialogContent>
           <DialogActions className={`${pokeModal}__actions`}>
-            <Button 
+            <Button
               onClick={verifyHasSelectedPokemon}
-              variant='contained'
-              color='warning'>Closed</Button>
+              variant="contained"
+              color="warning"
+            >
+              Closed
+            </Button>
           </DialogActions>
         </Dialog>
       </>
