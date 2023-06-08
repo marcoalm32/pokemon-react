fbe600026e5f61cdd3174317201a285e86320ee0
marcoalm32 <marcoalm25@gmail.com>
2023-06-08 18:39:30 -0300

    Created ImgType enum

---
+++ pokemon-react/src/models/enums/imgType.ts
@@ -0,0 +1,8 @@
+export enum ImgType {
+    FRONT="front",
+    BACK="back",
+    FRONT_SHINY="front_shiny",
+    BACK_SHINY="back_shiny",
+    FRONT_FEMALE="front_female",
+    BACK_FEMALE="back_female"
+}
