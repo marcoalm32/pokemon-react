747ed01edcd3812b14666d7771ce9e13b6974430
marcoalm32 <marcoalm25@gmail.com>
2023-06-08 19:10:38 -0300

    Implemented grid component in pokemon list

--- pokemon-react/src/components/navbar/Navbar.tsx
+++ pokemon-react/src/components/navbar/Navbar.tsx
@@ -9,6 +9,7 @@ import  { useNavigate } from 'react-router-dom';
 import './Navbar.scss';
 import { SearchPokemon } from '../search-pokemon/SearchPokemon';
 import useSearchPokemonStore from '../../store/searchStore';
+import Grid from '@mui/material/Grid';
 
 export const Navbar = () => {
 
@@ -25,24 +26,38 @@ export const Navbar = () => {
       <Box sx={{ flexGrow: 1 }}>
         <AppBar position="static" sx={{backgroundColor: '#e3f2fd'}}>
           <Toolbar className={navbar}>
-            <div className={`${navbar}__logo-and-title`}>
-                <IconButton
-                size="large"
-                color="inherit"
-                aria-label="menu"
-                sx={{ mr: 1 }}
-                >
-                <CatchingPokemonIcon />
-                </IconButton>
-                <span>Pokemon React App</span>
-            </div>
-            <SearchPokemon
-              pokemonFilter={(e: any) => pokemonFilter(e)}
-            />
-            <div className={`${navbar}__action`}>
-                <Button color="inherit" onClick={() => navigate("/")} >Home</Button>
-                <Button color='inherit' onClick={() => navigate("/pokemons")}>List Pokemons</Button>
-            </div>
+            <Grid container spacing={2}>
+              <Grid item xs={4} md={4}>
+                <div className={`${navbar}__logo-and-title`} >
+                    <IconButton
+                    size="large"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 1 }}
+                    >
+                    <CatchingPokemonIcon />
+                    </IconButton>
+                    <span>Pokemon React App</span>
+                </div>
+              </Grid>
+              <Grid item xs={4} md={4}>
+                <SearchPokemon
+                    pokemonFilter={(e: any) => pokemonFilter(e)}
+                  />
+              </Grid>
+              <Grid item xs={3} md={3}>
+                <div className={`${navbar}__action`}>
+                    <Button 
+                      sx={{fontWeight: '600'}} 
+                      color="inherit" 
+                      onClick={() => navigate("/")} >Home</Button>
+                    <Button 
+                      sx={{fontWeight: '600'}} 
+                      color='inherit' 
+                      onClick={() => navigate("/pokemons")}>List Pokemons</Button>
+                </div>
+              </Grid>
+            </Grid>
           </Toolbar>
         </AppBar>
       </Box>
