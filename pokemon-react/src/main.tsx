6e709805a8002dae6b2319dbaede5722f966df95
marcoalm32 <marcoalm25@gmail.com>
2023-06-10 13:07:27 -0300

    Created provider in main component

--- pokemon-react/src/main.tsx
+++ pokemon-react/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
+import { PokemonContextProvider } from './context/PokemonContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <PokemonContextProvider>
+      <App />
+    </PokemonContextProvider>
   </React.StrictMode>,
 )
